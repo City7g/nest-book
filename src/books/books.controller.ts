241f774e63306a50c732654eaa8c33ef36c46336
City7g <City7gor@gmail.com>
2024-10-02 20:29:53 +0300

    Add get request

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Book } from './book.entity';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
   @Get()
-  getBooks(): string {
-    return this.booksService.getBooks();
+  async getBooks(): Promise<Book[]> {
+    try {
+      return await this.booksService.getBooks();
+    } catch (error) {
+      console.error('Database error:', error);
+      throw new HttpException({ error: 'Server error' }, 500);
+    }
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './book.entity';
 
 @Injectable()
 export class BooksService {
-  getBooks(): string {
-    return 'All books';
+  constructor(
+    @InjectRepository(Book)
+    private booksRepository: Repository<Book>,
+  ) {}
+
+  async getBooks(): Promise<Book[]> {
+    return await this.booksRepository.find();
   }
 }
